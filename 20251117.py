3508e37a21248cf956dac153c7d0320634f3e5f5
Kim-Junghyeong <cxop2002@naver.com>
2025-11-18 17:13:37 +0900

    수정

--- 20251117.py
+++ 20251117.py
@@ -187,3 +187,55 @@ print(df_weather.info())
 print("------")
 print(df_weather.head(2))
 
+# 20251117_HomeWork_3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False
+
+data = {'부서': ['영업', '기획', '개발', '마케팅'],
+        '예산(만원)': [1500, 800, 2200, 1000],
+        }
+
+df= pd.DataFrame(data)
+
+df1 = df.sort_values(by='예산(만원)',ascending=False)
+print(df1)
+
+labels = df1['부서'].tolist()
+values = df1['예산(만원)'].tolist()
+
+plt.bar(labels,values)
+plt.grid()
+plt.title("부서별 예산 테이블")
+plt.xlabel("부서")
+plt.ylabel("예산(만원)")
+plt.show()
+
+# 20251117_HomeWork_4
+
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False
+
+data = {'분기': ['1분기', '2분기', '3분기', '4분기'],
+        '서울 매출 (억원)': [300, 350, 420, 380],
+        '부산 매출 (억원)': [250, 300, 380, 400]
+        }
+
+df = pd.DataFrame(data)
+
+x_values = df['분기'].tolist()
+y_values1 = df['서울 매출 (억원)'].tolist()
+y_values2 = df['부산 매출 (억원)'].tolist()
+
+plt.plot(x_values,y_values1, label='서울 매출')
+plt.plot(x_values,y_values2, label = '부산매출')
+plt.legend()
+plt.grid()
+plt.title("지역별 분기 매출 테이블")
+plt.xlabel("분기")
+plt.ylabel("매출 (억원)")
+plt.show()
